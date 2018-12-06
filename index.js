c2c7dfaab6471280e7e7deef3fc1a9a1e5ba8fca
Anno <annnoocodes@gmail.com>
2018-12-07 00:20:39 +0100

    heroku change

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 
 
